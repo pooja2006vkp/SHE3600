a0887cdbd773bd43cd4268f3b9900902831171a1
pooja2006vkp <kannanpoojak@gmail.com>
2026-05-15 09:19:17 +0530

    editted

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ cred = credentials.Certificate({
     "type": "service_account",
     "project_id": os.getenv('FIREBASE_PROJECT_ID'),
     "private_key_id": os.getenv('FIREBASE_PRIVATE_KEY_ID'),
-    "private_key": os.getenv('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
+    "private_key": (os.getenv('FIREBASE_PRIVATE_KEY') or "").replace('\\n', '\n'),
     "client_email": os.getenv('FIREBASE_CLIENT_EMAIL'),
     "client_id": os.getenv('FIREBASE_CLIENT_ID'),
     "auth_uri": "https://oauth2.googleapis.com/token",
